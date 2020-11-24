97b373fce4ca0cdc32fa47afdb8072a53093b42d
tekjar <raviteja@bytebeam.io>
2020-11-24 19:51:49 +0530

    Reorganize benchmarks and parser benchmarks

--- benchmarks/clients/clients.sh
+++ benchmarks/clients/clients.sh
@@ -4,6 +4,6 @@ set -e
 cargo run --bin rumqttasync --release | tee benchmarks.txt
 cargo run --bin rumqttsync --release | tee -a benchmarks.txt
 # cargo run --bin pahoasync --release | tee -a benchmarks.txt
-cargo run -q --bin pahosync --release | tee -a benchmarks.txt
+# cargo run -q --bin pahosync --release | tee -a benchmarks.txt
 go run paho.go | tee -a benchmarks.txt
 
---
+++ benchmarks/parsers/common.rs
@@ -0,0 +1,15 @@
+use pprof::{ProfilerGuard, protos::Message};
+use std::fs::File;
+use std::io::Write;
+
+#[allow(unused)]
+pub fn profile(name: &str, guard: ProfilerGuard) {
+    if let Ok(report) = guard.report().build() {
+        let mut file = File::create(name).unwrap();
+        let profile = report.pprof().unwrap();
+
+        let mut content = Vec::new();
+        profile.encode(&mut content).unwrap();
+        file.write_all(&content).unwrap();
+    };
+}
